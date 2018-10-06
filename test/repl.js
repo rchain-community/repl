7f766373879f6ef8853b1a87cf3ff03e2aa64d7d
Thomas Schoffelen <thomas@includable.com>
2018-10-06 18:38:49 +0200

    Add test framework

---
+++ test/repl.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const REPL = require('../lib/REPL')
+
+describe('eval', () => {
+    it('should be able to execute rholang', () => {
+      // FIXME
+      assert.equal(true, true)
+    })
+})
