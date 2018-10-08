2435c8ce70186d0e69c70d38eff5a7ec7ec62356
Thomas Schoffelen <thomas@includable.com>
2018-10-08 22:07:56 +0200

    Fixed a few bugs.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
-const path = require('path')
 const REPL = require('./lib/REPL')
 
 // Library mode:
 if (require.main !== module) {
+  module.exports = REPL
   return REPL
 }
 
